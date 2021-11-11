981ac9c78a251840ab9dbc218fa86ba080053aca
Jake Streb <jake@getgrist.com>
2021-11-11 01:52:20 -0500

    add env

--- api/request.js
+++ api/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.serverUrl;
+const baseUrl = process.env.SERVER_URL;
 
 export async function get(path, queryParams) {
 	try {
@@ -8,7 +8,7 @@ export async function get(path, queryParams) {
 			params: queryParams,
 			baseURL: baseUrl,
 			paramsSerializer: params => qs.stringify(params),
-			timeout: process.env.httpTimeout
+			timeout: process.env.API_TIMEOUT
 		});
 		return data;
 	} catch (err) {
@@ -22,7 +22,7 @@ export async function post(path, queryParams, body) {
 		const { data } = await axios.post(path, body, {
 			params: queryParams,
 			baseURL: baseUrl,
-			timeout: process.env.httpTimeout
+			timeout: process.env.API_TIMEOUT
 		});
 		console.warn('data', data);
 		return data;
--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: {
-    serverUrl: 'https://workout-catalog.herokuapp.com',
-    httpTimeout: 2000
-  },
-}
